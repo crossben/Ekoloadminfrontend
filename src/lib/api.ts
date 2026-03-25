281ff4e39cecbeec72a933c056282c793cf2f70e
crossben <gueco167@gmail.com>
2026-03-25 19:17:02 +0000

    chore: remove commented out API base URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api.ekolo.gerexatlas.com/api',
   baseURL: 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
